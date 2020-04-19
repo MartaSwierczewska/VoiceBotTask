55d90ded0143ff56bfda236c7baf5a7705be4c2e
Marta <martaswierczewska@wp.pl>
2020-04-19 16:05:35 +0200

    data extractor and mapper

---
+++ src/main/java/DataExtractor.java
@@ -0,0 +1,38 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataExtractor {
+
+    public List<Employee> getEmployeeListFromJsonFile(String filePath) throws IOException {
+        Employees employees = new ObjectMapper().readValue(Paths.get(filePath).toFile(), new TypeReference<Employees>() {});
+        return employees.getEmployees();
+    }
+
+    public List<Employee> getEmployeeListFromCSVFile(String filePath) {
+        EmployeeMapper employeeMapper = new EmployeeMapper();
+        List<Employee> employeeList = new ArrayList<>();
+        try (BufferedReader br = Files.newBufferedReader(Paths.get(filePath), StandardCharsets.UTF_8)) {
+//            header line needs to be skipped
+            br.readLine();
+            String line = br.readLine();
+            while (line != null) {
+                String[] attributes = line.split(";");
+                Employee employee = employeeMapper.createEmployee(attributes);
+                employeeList.add(employee);
+                line = br.readLine();
+            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        return employeeList;
+    }
+
+}
---
+++ src/main/java/EmployeeMapper.java
@@ -0,0 +1,23 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EmployeeMapper {
+    private static final Pattern pattern = Pattern.compile("\\s\"(.*?)\"");
+
+    public String executePatternOnMetadata(String data){
+        Matcher m = pattern.matcher(data);
+        if(m.find()) {
+            return (m.group(1));
+        }
+        return "";
+    }
+
+    public Employee createEmployee(String[] metadata) {
+        int id = Integer.parseInt(metadata[0].trim());
+        String name = executePatternOnMetadata(metadata[1]);
+        String surname = executePatternOnMetadata(metadata[2]);
+        String job = executePatternOnMetadata(metadata[3]);
+        String salary = executePatternOnMetadata(metadata[4]);
+        return new Employee(id,name,surname,job,salary);
+    }
+}
